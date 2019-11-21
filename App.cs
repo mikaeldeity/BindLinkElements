e15981fd43edce71ccac8b71855f980939ba7b7a
Mikael Santrolli <mikael.santrolli@gmail.com>
2019-11-21 12:58:34 +0000

    Add files via upload

---
+++ App.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace BindLinkElements
+{
+    public class App : IExternalApplication
+    {
+        static void AddRibbonPanel(UIControlledApplication application)
+        {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("Link Elements");
+
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;            
+            PushButtonData b1Data = new PushButtonData("Copy", "Copy", thisAssemblyPath, "BindLinkElements.Copy");
+            PushButton pb1 = ribbonPanel.AddItem(b1Data) as PushButton;
+            pb1.ToolTip = "Copy Elements from Link Instances to current Document.";
+            BitmapImage pb1Image = new BitmapImage(new Uri(thisAssemblyPath.Replace("BindLinkElements.dll", "") + "BindLinkElements.png"));
+            pb1.LargeImage = pb1Image;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddRibbonPanel(application);
+
+            return Result.Succeeded;
+        }
+    }
+}
